30c72597060e1ec0896f5cd81d58313e3fd51fde
Denis Burguto <65451582GMAIL.COM>
2024-05-02 10:43:23 +0300

    project structure

--- final/food/urls.py
+++ final/food/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-
+    path('authorization', views.authorization, name='authorization'),
+    path('all_receipt', views.all_receipt, name='all_receipt'),
+    path('get_receipt', views.get_receipt, name='get_receipt'),
+    path('add_receipt', views.add_receipt, name='add_receipt'),
+    path('modify_receipt', views.modify_receipt, name='modify_receipt'),
+    path('add_receipt/', views.add_receipt, name='add_receipt'),
 
 ]
--- final/food/views.py
+++ final/food/views.py
@@ -8,3 +8,21 @@ logger = logging.getLogger(__name__)
 def main(request):
     logger.info('Main page accessed')
     return render(request, "food/main.html")
+
+
+def authorization(request):
+    return render(request, "food/authorization.html")
+
+
+def add_receipt(request):
+    return render(request, "food/add_receipt.html")
+
+def modify_receipt(request):
+    return render(request, "food/modify_receipt.html")
+
+def all_receipt(request):
+    return render(request, "food/all_receipt.html")
+
+
+def get_receipt(request):
+    return render(request, "food/get_receipt.html")
