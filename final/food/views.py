48c3c091657625eb03f528091d6f5312b9ab34f0
Denis Burguto <65451582GMAIL.COM>
2024-05-02 23:22:44 +0300

    some upd in main

--- final/food/views.py
+++ final/food/views.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 def main(request):
-    recent_receipts = Receipt.objects.all()[:5]
+    recent_receipts = Receipt.objects.all().order_by('-id')[:5]
     logger.info('Main page accessed')
     return render(request, "food/main.html", {'recent_receipts': recent_receipts})
 
