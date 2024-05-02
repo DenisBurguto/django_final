2bee566c56a05e5758d0dd4690a5211840a9d2c1
Denis Burguto <65451582GMAIL.COM>
2024-05-02 12:08:23 +0300

    authorization

--- final/food/urls.py
+++ final/food/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('authorization', views.authorization, name='authorization'),
-    path('all_receipt', views.all_receipt, name='all_receipt'),
-    path('get_receipt', views.get_receipt, name='get_receipt'),
-    path('add_receipt', views.add_receipt, name='add_receipt'),
-    path('modify_receipt', views.modify_receipt, name='modify_receipt'),
+    path('authorization/', views.authorization, name='authorization'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.user_logout, name='logout'),
+    path('all_receipt/', views.all_receipt, name='all_receipt'),
+    path('get_receipt/', views.get_receipt, name='get_receipt'),
     path('add_receipt/', views.add_receipt, name='add_receipt'),
-
+    path('modify_receipt/', views.modify_receipt, name='modify_receipt'),
 ]
--- final/food/views.py
+++ final/food/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm, LoginForm
 import logging
 
 logger = logging.getLogger(__name__)
@@ -11,14 +13,44 @@ def main(request):
 
 
 def authorization(request):
-    return render(request, "food/authorization.html")
+    if request.method == 'POST':
+        form = LoginForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            logger.info(f'New loging {user.username}')
+            return redirect('main')
+    else:
+        form = LoginForm()
+    return render(request, 'food/authorization.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            logger.info('New registration')
+            return redirect('main')
+    else:
+        form = RegistrationForm()
+    return render(request, 'food/authorization.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    logger.info('logout')
+    return redirect('main')
 
 
 def add_receipt(request):
     return render(request, "food/add_receipt.html")
 
+
 def modify_receipt(request):
     return render(request, "food/modify_receipt.html")
+
 
 def all_receipt(request):
     return render(request, "food/all_receipt.html")
